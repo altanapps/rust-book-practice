9fd8d18857a2bb9983c7c04a44a5c02bc7365fb9
altanapps <futureisaltan@gmail.com>
2023-06-19 20:26:29 +0100

    added iterators to the minigrep project

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -8,13 +8,21 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item=String>) -> Result<Config, &'static str> {
+        // Skip the first argument, which is the name of the program
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        // Get the query and file path
+        let query = match args.next () {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next () {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config { query, file_path, ignore_case })
@@ -41,13 +49,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 // Lifetimes are still not cristal clear to me
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-    results
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(
